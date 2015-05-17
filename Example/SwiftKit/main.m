a9a340c0bb8cd1b9a4ee4d1d420c2c5f953d5a2f
Tadeas Kriz <tadeas@brightify.org>
2015-05-17 18:50:32 +0200

    Initial commit

---
+++ Example/SwiftKit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SwiftKit
+//
+//  Created by Tadeas Kriz on 05/17/2015.
+//  Copyright (c) 2014 Tadeas Kriz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BFYAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BFYAppDelegate class]));
+    }
+}
