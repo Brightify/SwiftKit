5673da4835c04c4246e3ba4b0e50e3249f861360
Matouš Hýbl <hyblmatous@gmail.com>
2015-06-28 20:52:38 +0200

    Fix single line comments

--- Preferences/Preference.swift
+++ Preferences/Preference.swift
@@ -8,21 +8,15 @@
 
 import Foundation
 
-/**
-* Protocol whose implementations help with accessing NSUserPreference
-*/
+/// Protocol whose implementations help with accessing NSUserDefaults
 public protocol Preference {
     
     typealias T
-    
-    /**
-    * Contains value of the Preference
-    */
+
+    /// Contains value of the Preference
     var value: T { get set }
     
-    /**
-    * This is true if the Preference with correct key and type exists
-    */
+    /// This is true if the Preference with correct key and type exists
     var exists: Bool { get }
     
     /**
@@ -32,9 +26,7 @@ public protocol Preference {
     */
     init(key: String, defaultValue: T)
     
-    /**
-    * Deletes the Preference from NSUserPreferences
-    */
+    /// Deletes the Preference from NSUserDefaults
     func delete()
     
 }
