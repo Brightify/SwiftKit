202f144ade865971fa0fe5c1ebfc57cccb7c0920
Tadeas Kriz <tadeas@brightify.org>
2016-01-09 18:56:43 +0100

    Edit some tests for ViewComposer.

--- Tests/ViewComposerTest.swift
+++ Tests/ViewComposerTest.swift
@@ -31,6 +31,8 @@ class ViewComposerTest: QuickSpec {
                 
                 expect(view.superview).to(equal(superview))
                 expect(optionalView?.superview).to(equal(superview))
+                
+                expect(superview.subviews.count).to(equal(2))
             }
             
             it("composes view and adds into superview using operator api") {
@@ -50,6 +52,8 @@ class ViewComposerTest: QuickSpec {
                 
                 expect(view.superview).to(equal(superview))
                 expect(optionalView?.superview).to(equal(superview))
+                
+                expect(superview.subviews.count).to(equal(2))
             }
             
             it("configures existing view and adds into superview using operator api") {
@@ -74,6 +78,34 @@ class ViewComposerTest: QuickSpec {
                 expect(view.superview).to(equal(superview))
                 expect(optionalView?.superview).to(equal(superview))
                 expect(implicitlyUnwrappedView?.superview).to(equal(superview))
+                
+                expect(superview.subviews.count).to(equal(3))
+            }
+            
+            it("configures existing view and adds into superview using fluent api") {
+                let superview = ViewComposer.compose(UIView).view
+                
+                let view: UIView = UIView()
+                let optionalView: UIView? = UIView()
+                let implicitlyUnwrappedView : UIView! = UIView()
+                
+                ViewComposer.configure(view).addInto(superview)
+                ViewComposer.configure(optionalView!).addInto(superview)
+                ViewComposer.configure(implicitlyUnwrappedView).addInto(superview)
+                
+                expect(view).toNot(beNil())
+                expect(optionalView).toNot(beNil())
+                expect(implicitlyUnwrappedView).toNot(beNil())
+                
+                expect(view.translatesAutoresizingMaskIntoConstraints).to(beFalse())
+                expect(optionalView?.translatesAutoresizingMaskIntoConstraints).to(beFalse())
+                expect(implicitlyUnwrappedView?.translatesAutoresizingMaskIntoConstraints).to(beFalse())
+                
+                expect(view.superview).to(equal(superview))
+                expect(optionalView?.superview).to(equal(superview))
+                expect(implicitlyUnwrappedView?.superview).to(equal(superview))
+                
+                expect(superview.subviews.count).to(equal(3))
             }
         }
     }
